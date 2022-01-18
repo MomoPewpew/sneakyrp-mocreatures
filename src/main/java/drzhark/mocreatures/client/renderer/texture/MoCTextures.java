f6a096ef95053156ef608aabd113f8c5b253ed07
Monica Moniot <mamoniot@protonmail.com>
2022-01-18 13:13:37 -0500

    prep

--- src/main/java/drzhark/mocreatures/client/renderer/texture/MoCTextures.java
+++ src/main/java/drzhark/mocreatures/client/renderer/texture/MoCTextures.java
@@ -1,5 +1,5 @@
 /*     */ package drzhark.mocreatures.client.renderer.texture;
-/*     */ 
+/*     */
 /*     */ import com.google.common.collect.Maps;
 /*     */ import drzhark.mocreatures.MoCProxy;
 /*     */ import java.io.File;
@@ -14,14 +14,14 @@
 /*     */ import java.util.jar.JarEntry;
 /*     */ import java.util.jar.JarFile;
 /*     */ import net.minecraft.util.ResourceLocation;
-/*     */ 
-/*     */ 
+/*     */
+/*     */
 /*     */ public class MoCTextures
 /*     */ {
 /*  21 */   private static final Map<String, ResourceLocation> RESOURCE_CACHE = Maps.newHashMap();
-/*  22 */   private static final Map<String, String[]> TEXTURE_RESOURCES = Maps.newHashMap();
-/*     */   
+/*     */
 /*     */   public void loadTextures() {
+/*  22 */   Map<String, String[]> TEXTURE_RESOURCES = Maps.newHashMap();
 /*     */     try {
 /*  26 */       TEXTURE_RESOURCES.put(MoCProxy.ARMOR_TEXTURE, getResourceListing(getClass(), "assets/mocreatures/textures/armor/"));
 /*  27 */       TEXTURE_RESOURCES.put(MoCProxy.BLOCK_TEXTURE, getResourceListing(getClass(), "assets/mocreatures/textures/blocks/"));
@@ -33,7 +33,7 @@
 /*  33 */       e.printStackTrace();
 /*  34 */     } catch (IOException e) {
 /*  35 */       e.printStackTrace();
-/*     */     } 
+/*     */     }
 /*  37 */     for (Map.Entry<String, String[]> textureEntry : TEXTURE_RESOURCES.entrySet()) {
 /*  38 */       String[] resources = textureEntry.getValue();
 /*  39 */       if (resources != null && resources.length > 0) {
@@ -41,43 +41,43 @@
 /*  41 */           if (resources[i].contains(".png")) {
 /*  42 */             RESOURCE_CACHE.put(resources[i], new ResourceLocation("mocreatures", (String)textureEntry.getKey() + resources[i]));
 /*     */           }
-/*     */         } 
+/*     */         }
 /*     */       }
-/*     */     } 
+/*     */     }
 /*     */   }
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */   
+/*     */
+/*     */
+/*     */
+/*     */
+/*     */
+/*     */
+/*     */
+/*     */
+/*     */
+/*     */
+/*     */
+/*     */
+/*     */
+/*     */
+/*     */
 /*     */   String[] getResourceListing(Class clazz, String path) throws URISyntaxException, IOException {
 /*  64 */     URL dirURL = clazz.getClassLoader().getResource(path);
 /*  65 */     if (dirURL != null && dirURL.getProtocol().equals("file"))
 /*     */     {
 /*  67 */       return (new File(dirURL.toURI())).list();
 /*     */     }
-/*     */     
+/*     */
 /*  70 */     if (dirURL == null) {
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */       
+/*     */
+/*     */
+/*     */
+/*     */
 /*  75 */       String me = clazz.getName().replace(".", "/") + ".class";
 /*  76 */       dirURL = clazz.getClassLoader().getResource(me);
-/*     */     } 
-/*     */     
+/*     */     }
+/*     */
 /*  79 */     if (dirURL.getProtocol().equals("jar")) {
-/*     */       
+/*     */
 /*  81 */       String jarPath = dirURL.getPath().substring(5, dirURL.getPath().indexOf("!"));
 /*  82 */       JarFile jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"));
 /*  83 */       Enumeration<JarEntry> entries = jar.entries();
@@ -92,15 +92,15 @@
 /*  92 */             entry = entry.substring(0, checkSubdir);
 /*     */           }
 /*  94 */           result.add(entry);
-/*     */         } 
-/*     */       } 
+/*     */         }
+/*     */       }
 /*  97 */       jar.close();
 /*  98 */       return result.<String>toArray(new String[result.size()]);
-/*     */     } 
-/*     */     
+/*     */     }
+/*     */
 /* 101 */     throw new UnsupportedOperationException("Cannot list files for URL " + dirURL);
 /*     */   }
-/*     */   
+/*     */
 /*     */   public ResourceLocation getTexture(String texture) {
 /* 105 */     return RESOURCE_CACHE.get(texture);
 /*     */   }
